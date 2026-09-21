b5a4e3b8ae6b125a084dd2af90539fbee81a3798
Duy Cao <thienduy1295@gmail.com>
2026-09-21 12:30:03 +0700

    service verfy session at backend

--- ecommerce-backend/src/app/auth/auth.module.ts
+++ ecommerce-backend/src/app/auth/auth.module.ts
@@ -3,8 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
 import { Session } from './entities/session.entity';
 import { Verification } from './entities/verification.entity';
+import { SessionAuthService } from './session-auth/session-auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account, Session, Verification])],
+  providers: [SessionAuthService],
+  exports: [SessionAuthService],
 })
 export class AuthModule {}
